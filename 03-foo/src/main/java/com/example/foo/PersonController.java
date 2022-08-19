3e5bb35ac01934289f67aef7862759f5c85c9f45
“R90” <rbc07@jfpr.local>
2022-08-19 18:23:20 -0300

    api

--- 03-foo/src/main/java/com/example/foo/PersonController.java
+++ 03-foo/src/main/java/com/example/foo/PersonController.java
@@ -1,6 +1,9 @@
 package com.example.foo;
 
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.foo.exception.UnsuportedMathException;
 import com.example.foo.model.Person;
 import com.example.foo.services.PersonService;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.histquotes2.IntervalMapper;
 
 @RestController
 @RequestMapping("/person")
@@ -51,6 +60,34 @@ public class PersonController {
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll(){
 		return services.findAll();
+	}
+	
+	@RequestMapping(value="stock/{id}" , method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<HistoricalQuote> stock(@PathVariable(value = "id") String id){
+		
+		Stock stock;
+		String result = "";
+		List<HistoricalQuote> precos = new ArrayList<HistoricalQuote>();
+		try {
+			stock = YahooFinance.get(id);
+		
+
+			BigDecimal price = stock.getQuote().getPrice();
+			BigDecimal change = stock.getQuote().getChangeInPercent();
+			BigDecimal peg = stock.getStats().getPeg();
+			BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+	
+			stock.print();
+			Interval i = Interval.MONTHLY;
+			//result = stock.getName() + " " + price.toString() + " "+stock.getHistory(Interval.MONTHLY).toString();
+			precos = stock.getHistory(Interval.MONTHLY);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return precos;
 	}
 	
 	
